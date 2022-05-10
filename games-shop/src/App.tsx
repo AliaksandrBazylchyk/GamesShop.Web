b6887b10c2c8a2a508aaf1980d57d49ddaa9ce77
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-05-10 16:10:52 +0300

    Configured routes

--- games-shop/src/App.tsx
+++ games-shop/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Routes } from 'react-router-dom'
+
 import './App.css'
 
 const App: React.FC = () => {
-    return <div className="App" />
+    return (
+        <div className="App">
+            <Routes />
+        </div>
+    )
 }
 
 export default App
