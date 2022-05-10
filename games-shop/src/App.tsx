aff7dac11340baa9c85e0c8187c5212ce1c103c7
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-05-10 17:00:42 +0300

    Configured path aliases

--- games-shop/src/App.tsx
+++ games-shop/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import Home from '@pages/Home/Home'
+
 import './App.css'
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <Routes>
-                <Route path="/" element="" />
+                <Route path="/" element={<Home />} />
             </Routes>
         </div>
     )
