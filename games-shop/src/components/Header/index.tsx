4ca0b7b7d9be9e897cb369bf7c67711a671e30a3
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-05-24 15:56:12 +0300

    Configured routes for header

--- games-shop/src/components/Header/index.tsx
+++ games-shop/src/components/Header/index.tsx
@@ -1,24 +1,62 @@
 import React from 'react'
 
-import { AppBar, Button, Toolbar, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+
+import {
+    AppBar,
+    Box,
+    Button,
+    Toolbar,
+    Typography,
+    useTheme,
+} from '@mui/material'
 import LoginIcon from '@mui/icons-material/Login'
 import GroupAddIcon from '@mui/icons-material/GroupAdd'
 
 const Header: React.FC = () => {
+    const theme = useTheme()
+
     return (
         <AppBar>
             <Toolbar>
-                <Typography variant="h1" component="div" sx={{ flexGrow: 1 }}>
-                    Shop
+                <Typography
+                    variant="h1"
+                    component="div"
+                    sx={{ flexGrow: 1, textDecoration: 'none' }}
+                >
+                    <Link
+                        to="/"
+                        style={{
+                            textDecoration: 'none',
+                            color: theme.palette.text.primary,
+                        }}
+                    >
+                        Shop
+                    </Link>
                 </Typography>
-                <Button>
-                    Register
-                    <GroupAddIcon sx={{ marginLeft: 1 }} />
-                </Button>
-                <Button>
-                    Login
-                    <LoginIcon sx={{ marginLeft: 1 }} />
-                </Button>
+
+                <Link
+                    to="/register"
+                    style={{
+                        textDecoration: 'none',
+                    }}
+                >
+                    <Button>
+                        Register
+                        <GroupAddIcon sx={{ marginLeft: 1 }} />
+                    </Button>
+                </Link>
+                <Link
+                    to="/login"
+                    style={{
+                        textDecoration: 'none',
+                    }}
+                >
+                    <Button>
+                        Login
+                        <LoginIcon sx={{ marginLeft: 1 }} />
+                    </Button>
+                </Link>
             </Toolbar>
         </AppBar>
     )
